311c56bbd080912668d8598e158a4a0ac39463a8
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2026-01-27 13:55:07 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import Database from "./classes/database";
-import Quote from "./classes/quote";
 import QuotoMoto from "./classes/quotomoto";
 import { migrations } from "./migrations";
 
 await Database.migrate("quotomoto.db", migrations);
-
-new Quote().render();
 
 new QuotoMoto();
